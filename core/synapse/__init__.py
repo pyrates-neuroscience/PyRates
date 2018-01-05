6331dbe40f9247c828a2cb31703f2e10f5bcd235
Richard Gast <richgast.90@gmail.com>
2018-01-05 16:29:49 +0100

    Fixed MoranCircuit to take max_synaptic_delay as optional argument

--- core/synapse/__init__.py
+++ core/synapse/__init__.py
@@ -8,3 +8,4 @@ from .synapse import Synapse, DoubleExponentialSynapse, ExponentialSynapse
 from .templates import AMPAConductanceSynapse, AMPACurrentSynapse
 from .templates import GABAAConductanceSynapse, GABAACurrentSynapse
 from .templates import JansenRitExcitatorySynapse, JansenRitInhibitorySynapse
+from .templates import MoranExcitatorySynapse, MoranInhibitorySynapse
