e2b30c6c04dc5c9931ea99c64d4ff766b5b03b9b
Daniel Rose <drose@cbs.mpg.de>
2018-07-09 14:01:11 +0200

    omplemented loading of node templates

---
+++ pyrates/abc.py
@@ -0,0 +1,24 @@
+""" Abstract base classes
+"""
+
+__author__ = "Daniel Rose"
+__status__ = "Development"
+
+
+class AbstractBaseTemplate:
+    """Abstract base class for templates"""
+
+    def __init__(self, name: str, path: str, description: str):
+        self.name = name
+        self.path = path
+        self.__doc__ = description
+
+    def __repr__(self):
+        return f"{self.__class__.__name__} <{self.path}>"
+
+    def _format_path(self, path):
+        """Check if path contains a folder structure and prepend own path, if it doesn't"""
+
+        if "." not in path:
+            path = f"{self.path.split('.')[:-1]}.{path}"
+        return path
--- tests/core/test_yaml_parser.py
+++ tests/core/test_yaml_parser.py
@@ -1,6 +1,8 @@
 """ Tests for the parser that translates circuits and components defined in YAML into the intermediate
 python representation.
 """
+from pyrates.node import NodeTemplate
+from pyrates.operator import OperatorTemplate
 
 __author__ = "Daniel Rose"
 __status__ = "Development"
@@ -16,7 +18,7 @@ def setup_module():
 
 
 @pytest.mark.parametrize("section", ["axon", "population", "synapse"])
-def test_parse_basic_templates(section):
+def test_parse_operator_templates(section):
     """test basic (vanilla) YAML parsing using ruamel.yaml (for YAML 1.2 support)"""
     from ruamel.yaml import YAML
     import os
@@ -44,17 +46,39 @@ def test_parse_basic_templates(section):
                                       "pyrates.population.population.CurrentToPotentialOperator",
                                       "pyrates.synapse.synapse.RateToCurrentOperator"
                                       ])
-def test_import_templates(operator):
+def test_import_operator_templates(operator):
     """test basic (vanilla) YAML parsing using ruamel.yaml (for YAML 1.2 support)"""
-    from pyrates.utility.yaml_parser import TemplateLoader
+    from pyrates.utility.yaml_parser import OperatorTemplateLoader
 
-    template = TemplateLoader(operator)
+    template = OperatorTemplateLoader(operator)  # type: OperatorTemplate
 
-    assert template.path in TemplateLoader.cache
+    assert template.path in OperatorTemplateLoader.cache
 
-    cached_template = TemplateLoader.cache[template.path]
+    cached_template = OperatorTemplateLoader.cache[operator]
     assert template is cached_template
     assert template.path == cached_template.path
     assert template.equations == cached_template.equations
-    assert repr(template) == repr(cached_template)
+    assert repr(template) == repr(cached_template) == f"OperatorTemplate <{operator}>"
     # assert template == TemplateLoader.cache[template.path]
+
+
+@pytest.mark.parametrize("node", ["pyrates.population.templates.JansenRitIN",
+                                  "pyrates.population.templates.JansenRitPC",
+                                  "pyrates.population.population.NeuralMass"
+                                  ])
+def test_import_node_templates(node):
+    """test import of node templates"""
+
+    from pyrates.utility.yaml_parser import NodeTemplateLoader, TemplateLoader
+
+    template = NodeTemplateLoader(node)  # type: NodeTemplate
+
+    assert template.path in TemplateLoader.cache
+
+    cached_template = NodeTemplateLoader.cache[node]
+    assert template is cached_template
+    assert template.path == cached_template.path
+    assert template.operators == cached_template.operators
+    assert repr(template) == repr(cached_template) == f"NodeTemplate <{node}>"
+
+
