55b7cbde1cb6a1702655726421078367e2b31b49
Richard Gast <richgast.90@gmail.com>
2018-10-16 15:37:43 +0200

    - debugged parser tests to pass with new parser module

--- tests/core/test_parser.py
+++ tests/core/test_parser.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import pytest
 
 # pyrates internal imports
-from pyrates.parser import TFExpressionParser
+from pyrates.parser import TFExpressionParser, parse_equation
 
 # meta infos
 __author__ = "Richard Gast, Daniel Rose"
@@ -39,7 +39,7 @@ def test_1_1_expression_parser_init():
     :class:`NPExpressionParser`: Documentation of the numpy-based expression parser
     """
 
-    # parsers = [NPExpressionParser, TFExpressionParser]
+    # list parsers to be tested
     parsers = [TFExpressionParser]
 
     # test minimal minimal call example
@@ -50,7 +50,6 @@ def test_1_1_expression_parser_init():
         assert isinstance(parser, Parser)
 
 
-@pytest.mark.xfail
 def test_1_2_expression_parser_parsing_exceptions():
     """Testing error handling of different erroneous parser instantiations:
 
@@ -60,7 +59,7 @@ def test_1_2_expression_parser_parsing_exceptions():
     :class:`LambdaExpressionParser`: Documentation of a non-symbolic expression parser.
     """
 
-    # parsers = [NPExpressionParser, TFExpressionParser]
+    # list parsers to be tested
     parsers = [TFExpressionParser]
 
     # test expected exceptions
@@ -70,26 +69,29 @@ def test_1_2_expression_parser_parsing_exceptions():
 
         # undefined variables
         with pytest.raises(ValueError):
-            Parser("a + b", {}).parse_expr()()
+            Parser("a + b", {}).parse_expr()
 
         # wrong dimensionality of dictionary arguments
         with pytest.raises(ValueError):
-            Parser("a + b", {'a': np.ones((3, 3)), 'b': np.ones((4, 4))}).parse_expr()()
+            Parser("a + b", {'a': {'var': np.ones((3, 3)), 'dependency': False},
+                             'b': {'var': np.ones((4, 4)), 'dependency': False}}).parse_expr()
 
         # wrong data type of dictionary arguments
         with pytest.raises(TypeError):
-            Parser("bool(a) + float32(b)", {'a': np.ones((3, 3)), 'b': np.ones((3, 3))}).parse_expr()()
+            Parser("bool(a) + float32(b)", {'a': {'var': np.ones((3, 3)), 'dependency': False},
+                                            'b': {'var': np.ones((3, 3)), 'dependency': False}}).parse_expr()()
 
         # undefined mathematical operator
         with pytest.raises(ValueError):
-            Parser("a $ b", {'a': np.ones((3, 3)), 'b': 5.}).parse_expr()()
+            Parser("a $ b", {'a': {'var': np.ones((3, 3)), 'dependency': False},
+                             'b': {'var': 5., 'dependency': False}}).parse_expr()()
 
         # undefined function
-        with pytest.raises(ValueError):
-            Parser("a / b(5.)", {'a': np.ones((3, 3)), 'b': 5.}).parse_expr()()
+        with pytest.raises(KeyError):
+            Parser("a / b(5.)", {'a': {'var': np.ones((3, 3)), 'dependency': False},
+                                 'b': {'var': 5., 'dependency': False}}).parse_expr()()
 
 
-@pytest.mark.xfail
 def test_1_3_expression_parser_math_ops():
     """Testing handling of mathematical operations by expression parsers:
 
@@ -117,21 +119,16 @@ def test_1_3_expression_parser_math_ops():
 
     for expr, target in math_expressions:
 
-        # # numpy-based parser
-        # result = NPExpressionParser(expr_str=expr, args={}).parse_expr()()
-        # assert result == pytest.approx(target, rel=1e-6)
-
         # tensorflow-based parser
         gr = tf.get_default_graph()
         with gr.as_default():
-            result = TFExpressionParser(expr_str=expr, args={}, tf_graph=gr).parse_expr()
+            result, _ = TFExpressionParser(expr_str=expr, args={}, tf_graph=gr).parse_expr()
         with tf.Session(graph=gr) as sess:
             sess.run(result)
             result = result.eval()
         assert result == pytest.approx(target, rel=1e-6)
 
 
-@pytest.mark.xfail
 def test_1_4_expression_parser_logic_ops():
     """Testing handling of logical operations by expression parsers:
 
@@ -157,13 +154,10 @@ def test_1_4_expression_parser_logic_ops():
 
     for expr in logic_expressions:
 
-        # # numpy-based parser
-        # assert NPExpressionParser(expr_str=expr, args={}).parse_expr()()
-
         # tensorflow-based parser
         gr = tf.get_default_graph()
         with gr.as_default():
-            result = TFExpressionParser(expr_str=expr, args={}, tf_graph=gr).parse_expr()
+            result, _ = TFExpressionParser(expr_str=expr, args={}, tf_graph=gr).parse_expr()
         with tf.Session(graph=gr) as sess:
             sess.run(result)
             assert result.eval()
@@ -173,19 +167,15 @@ def test_1_4_expression_parser_logic_ops():
 
     expr = "5 >= 6"
 
-    # # numpy-based parser
-    # assert not NPExpressionParser(expr_str=expr, args={}).parse_expr()()
-
     # tensorflow-based parser
     gr = tf.get_default_graph()
     with gr.as_default():
-        result = TFExpressionParser(expr_str=expr, args={}, tf_graph=gr).parse_expr()
+        result, _ = TFExpressionParser(expr_str=expr, args={}, tf_graph=gr).parse_expr()
     with tf.Session(graph=gr) as sess:
         sess.run(result)
         assert not result.eval()
 
 
-@pytest.mark.xfail
 def test_1_5_expression_parser_indexing():
     """Testing handling of indexing operations by expression parsers:
 
@@ -219,15 +209,14 @@ def test_1_5_expression_parser_indexing():
 
     for expr, target in indexed_expressions:
 
-        # # numpy-based parser
-        # result = NPExpressionParser(expr_str=expr, args={'A': A, 'B': np.where(B)}).parse_expr()()
-        # assert result == pytest.approx(target, rel=1e-6)
-
         # tensorflow-based parser
         gr = tf.get_default_graph()
         with gr.as_default():
-            result = TFExpressionParser(expr_str=expr, args={'A': tf.constant(A), 'B': tf.constant(np.argwhere(B))},
-                                        tf_graph=gr).parse_expr()
+            result, _ = TFExpressionParser(expr_str=expr, args={'A': {'var': tf.constant(A),
+                                                                      'dependency': False},
+                                                                'B': {'var': tf.constant(np.argwhere(B)),
+                                                                      'dependency': False}},
+                                           tf_graph=gr).parse_expr()
         with tf.Session(graph=gr) as sess:
             sess.run(result)
             result = result.eval()
@@ -247,19 +236,18 @@ def test_1_5_expression_parser_indexing():
     ###############################################
 
     for expr in indexed_expressions_wrong:
-        # numpy-based parser
-        # with pytest.raises((IndexError, ValueError, SyntaxError, TypeError)):
-        #     NPExpressionParser(expr_str=expr, args={'A': A, 'B': np.where(B)}).parse_expr()()
 
         # tensorflow-based parser
         gr = tf.get_default_graph()
         with gr.as_default():
             with pytest.raises((IndexError, ValueError, SyntaxError, TypeError)):
-                TFExpressionParser(expr_str=expr, args={'A': tf.constant(A), 'B': tf.constant(np.argwhere(B))},
+                TFExpressionParser(expr_str=expr, args={'A': {'var': tf.constant(A),
+                                                              'dependency': False},
+                                                        'B': {'var': tf.constant(np.argwhere(B)),
+                                                              'dependency': False}},
                                    tf_graph=gr).parse_expr()
 
 
-@pytest.mark.xfail
 def test_1_6_expression_parser_funcs():
     """Testing handling of function calls by expression parsers:
 
@@ -289,16 +277,11 @@ def test_1_6_expression_parser_funcs():
 
     for expr, target in expressions:
 
-        # # numpy-based parser
-        # result = NPExpressionParser(expr_str=expr, args={'A': A}).parse_expr()
-        # if callable(result):
-        #     result = result()
-        # assert result == pytest.approx(target, rel=1e-6)
-
         # tensorflow-based parser
         gr = tf.get_default_graph()
         with gr.as_default():
-            result = TFExpressionParser(expr_str=expr, args={'A': tf.constant(A)}, tf_graph=gr).parse_expr()
+            result, _ = TFExpressionParser(expr_str=expr, args={'A': {'var': tf.constant(A), 'dependency': False}},
+                                           tf_graph=gr).parse_expr()
         with tf.Session(graph=gr) as sess:
             sess.run(result)
             result = result.eval()
@@ -317,114 +300,102 @@ def test_1_6_expression_parser_funcs():
     ###############################################
 
     for expr in expressions_wrong:
-        # # numpy-based parser
-        # with pytest.raises((IndexError, ValueError, SyntaxError, TypeError)):
-        #     NPExpressionParser(expr_str=expr, args={'A': A}).parse_expr()()
 
         # tensorflow-based parser
         gr = tf.get_default_graph()
         with gr.as_default():
             with pytest.raises((IndexError, ValueError, SyntaxError, TypeError)):
-                TFExpressionParser(expr_str=expr, args={'A': tf.constant(A)},
+                TFExpressionParser(expr_str=expr, args={'A': {'var': tf.constant(A), 'dependency': False}},
                                    tf_graph=gr).parse_expr()
 
 
-@pytest.mark.skip
-def test_1_7_solver_init():
-    """Testing initializations of different equation solvers:
+# def test_1_7_solver_init():
+#     """Testing initializations of different equation solvers:
+#
+#     See Also
+#     --------
+#     :class:`Solver`: Detailed documentation of solver attributes and methods.
+#     """
+#
+#     solvers = [NPSolver, TFSolver]
+#
+#     # test minimal minimal call example
+#     ###################################
+#
+#     for Solver in solvers:
+#         solver = Solver(5., np.zeros(shape=()))
+#         assert isinstance(solver, Solver)
+
+
+# def test_1_8_solver_update():
+#     """Testing variable updates performed by solvers:
+#
+#     See Also
+#     --------
+#     :class:`Solver`: Detailed documentation of solver attributes and methods.
+#     """
+#
+#     # simple update
+#     ###############
+#
+#     var = np.ones(shape=(), dtype=np.float32)
+#     new_val = 5.
+#
+#     # numpy-based solver
+#     #upd = NPSolver(new_val, var).solve()
+#     #for u in upd:
+#     #    var_new = u()
+#     #assert var_new == new_val
+#
+#     # tensorflow-based solver
+#     gr = tf.Graph()
+#     with gr.as_default():
+#         v1 = tf.Variable(var, name='v1')
+#         solver = TFSolver(tf.constant(new_val), v1, tf_graph=gr)
+#         update = solver.solve()
+#         state_var = solver.state_var
+#         op = state_var.assign(update)
+#     with tf.Session(graph=gr) as sess:
+#         sess.run(tf.global_variables_initializer())
+#         sess.run(op)
+#         var_new = v1.eval()
+#     assert var_new == new_val
+#
+#     # integration
+#     #############
+#
+#     var = np.ones(shape=(), dtype=np.float32)
+#     new_val = 5.
+#     dt = 0.1
+#
+#     # numpy-based solver
+#     #upd = NPSolver(new_val, var, dt=dt).solve()
+#     #for u in upd:
+#     #    var_new = u()
+#     #assert var_new == pytest.approx(var + new_val * dt, rel=1e-6)
+#
+#     # tensorflow-based solver
+#     gr = tf.Graph()
+#     with gr.as_default():
+#         v1 = tf.Variable(var, name='v1')
+#         solver = TFSolver(tf.constant(new_val), v1, dt=dt, tf_graph=gr)
+#         update = solver.solve()
+#         state_var = solver.state_var
+#         op = state_var.assign(update)
+#     with tf.Session(graph=gr) as sess:
+#         sess.run(tf.global_variables_initializer())
+#         sess.run(op)
+#         var_new = v1.eval()
+#     assert var_new == 1 + dt * new_val
+
+
+def test_1_7_equation_parsing():
+    """Tests equation parsing functionalities.
 
     See Also
     --------
-    :class:`Solver`: Detailed documentation of solver attributes and methods.
+    :func:`parse_equation`: Detailed documentation of parse_equation arguments.
     """
-
-    solvers = [NPSolver, TFSolver]
-
-    # test minimal minimal call example
-    ###################################
-
-    for Solver in solvers:
-        solver = Solver(5., np.zeros(shape=()))
-        assert isinstance(solver, Solver)
-
-
-@pytest.mark.skip
-def test_1_8_solver_update():
-    """Testing variable updates performed by solvers:
-
-    See Also
-    --------
-    :class:`Solver`: Detailed documentation of solver attributes and methods.
-    """
-
-    # simple update
-    ###############
-
-    var = np.ones(shape=(), dtype=np.float32)
-    new_val = 5.
-
-    # numpy-based solver
-    #upd = NPSolver(new_val, var).solve()
-    #for u in upd:
-    #    var_new = u()
-    #assert var_new == new_val
-
-    # tensorflow-based solver
-    gr = tf.Graph()
-    with gr.as_default():
-        v1 = tf.Variable(var, name='v1')
-        solver = TFSolver(tf.constant(new_val), v1, tf_graph=gr)
-        update = solver.solve()
-        state_var = solver.state_var
-        op = state_var.assign(update)
-    with tf.Session(graph=gr) as sess:
-        sess.run(tf.global_variables_initializer())
-        sess.run(op)
-        var_new = v1.eval()
-    assert var_new == new_val
-
-    # integration
-    #############
-
-    var = np.ones(shape=(), dtype=np.float32)
-    new_val = 5.
-    dt = 0.1
-
-    # numpy-based solver
-    #upd = NPSolver(new_val, var, dt=dt).solve()
-    #for u in upd:
-    #    var_new = u()
-    #assert var_new == pytest.approx(var + new_val * dt, rel=1e-6)
-
-    # tensorflow-based solver
-    gr = tf.Graph()
-    with gr.as_default():
-        v1 = tf.Variable(var, name='v1')
-        solver = TFSolver(tf.constant(new_val), v1, dt=dt, tf_graph=gr)
-        update = solver.solve()
-        state_var = solver.state_var
-        op = state_var.assign(update)
-    with tf.Session(graph=gr) as sess:
-        sess.run(tf.global_variables_initializer())
-        sess.run(op)
-        var_new = v1.eval()
-    assert var_new == 1 + dt * new_val
-
-
-@pytest.mark.skip
-def test_1_9_equation_parser():
-    """Tests equation parser functionalities.
-
-    See Also
-    --------
-    :class:`EquationParser`: Detailed documentation of equation parser attributes and methods.
-    """
-
-    # test minimal minimal call example
-    ###################################
-
-    parser = EquationParser("a = 5. + 2.", {}, engine="tensorflow")
-    assert isinstance(parser, EquationParser)
 
     # define test equations
     #######################
@@ -432,8 +403,9 @@ def test_1_9_equation_parser():
     equations = ["a = 5. + 2.",              # simple update of variable
                  "d/dt * a = 5. + 2.",       # simple differential equation
                  ]
-    arguments = [{'a': np.zeros(shape=(), dtype=np.float32)},
-                 {'a': np.zeros(shape=(), dtype=np.float32), 'dt': 0.1}
+    arguments = [{'a': {'var': np.zeros(shape=(), dtype=np.float32), 'dependency': False}},
+                 {'a': {'var': np.zeros(shape=(), dtype=np.float32), 'dependency': False},
+                  'dt': {'var': 0.1, 'dependency': False}}
                  ]
     results = [7., 0.7]
 
@@ -442,21 +414,16 @@ def test_1_9_equation_parser():
 
     for eq, args, target in zip(equations, arguments, results):
 
-        # numpy-based parsing
-        #parser = EquationParser(eq, args, engine="numpy")
-        #result = parser.lhs_update[0]()
-        #assert result == pytest.approx(target, rel=1e-6)
-
         # tensorflow-based parsing
         gr = tf.Graph()
         with gr.as_default():
-            v = tf.Variable(args['a'])
-            args['a'] = v
-            parser = EquationParser(eq, args, engine="tensorflow")
-            if hasattr(parser, 'update'):
-                update = parser.target_var.assign(parser.update)
+            v = tf.Variable(args['a']['var'])
+            args['a']['var'] = v
+            (tf_var, tf_op), _ = parse_equation(equation=eq, args=args, tf_graph=gr)
+            if tf_op is None:
+                update = tf_var
             else:
-                update = parser.target_var
+                update = tf_var.assign(tf_op)
         with tf.Session(graph=gr) as sess:
             sess.run(tf.global_variables_initializer())
             sess.run(update)
