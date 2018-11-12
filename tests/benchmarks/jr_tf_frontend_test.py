f82184ec52a0bdb73fbf5e18d4b55c6007b049e9
Richard Gast <richgast.90@gmail.com>
2018-11-12 17:45:22 +0100

    debugged problems with coupling operators from frontend

--- tests/benchmarks/jr_tf_frontend_test.py
+++ tests/benchmarks/jr_tf_frontend_test.py
@@ -3,14 +3,14 @@ from pyrates.backend import ComputeGraph
 from matplotlib.pyplot import *
 
 # parameters
-n_jrcs = 1
+n_jrcs = 3
 
 # circuit IR setup
 circuit_temp = CircuitTemplate.from_yaml("pyrates.frontend.circuit.templates.JansenRitCircuit")
 circuits = {}
 for n in range(n_jrcs):
     circuits['jrc.' + str(n)] = circuit_temp
-circuit_ir = CircuitIR.from_circuits('jrc_net', circuits=circuits).network_def()
+circuit_ir = CircuitIR.from_circuits('jrc_net', circuits=circuits).network_def(revert_node_names=True)
 
 # create backend
 net = ComputeGraph(circuit_ir, vectorize='none')
@@ -18,9 +18,9 @@ inp_pc = np.zeros((1000, n_jrcs))
 inp_pc[:, 0] = 220. + np.random.randn(1000)
 inp_in = np.zeros((1000, n_jrcs))
 results, _ = net.run(1., outputs={'v': ('all', 'JansenRitPRO.0', 'V')},
-                     inputs={('jrc.0/JR_PC', 'JansenRitExcitatorySynapseRCO.0', 'u'): inp_pc,
-                             ('jrc.0/JR_II', 'JansenRitExcitatorySynapseRCO.0', 'u'): inp_in,
-                             ('jrc.0/JR_EI', 'JansenRitExcitatorySynapseRCO.0', 'u'): inp_in})
+                     inputs={('JR_PC', 'JansenRitExcitatorySynapseRCO.0', 'u'): inp_pc,
+                             ('JR_IIN', 'JansenRitExcitatorySynapseRCO.0', 'u'): inp_in,
+                             ('JR_EIN', 'JansenRitExcitatorySynapseRCO.0', 'u'): inp_in})
 results.pop('time')
 results.plot()
 show()
