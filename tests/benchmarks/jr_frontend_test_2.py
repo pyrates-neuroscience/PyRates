6fe125f2de9598fda561a744c1e9ce9a519a3c31
Daniel Rose <drose@cbs.mpg.de>
2018-11-13 14:36:29 +0100

    added multi-circuit variant

--- tests/benchmarks/jr_frontend_test_2.py
+++ tests/benchmarks/jr_frontend_test_2.py
@@ -1,12 +1,16 @@
-from pyrates.frontend.circuit import CircuitTemplate
+from pyrates.frontend.circuit import CircuitTemplate, CircuitIR
 from pyrates.backend import ComputeGraph
 import matplotlib.pyplot as plt
 
 circuit = CircuitTemplate.from_yaml("pyrates.examples.jansen_rit.JRC").apply()
 
-nd = circuit.network_def()
+c = CircuitIR()
+for i in range(4):
+    c.add_circuit(f"jrc.{i}", circuit)
 
-compute_graph = ComputeGraph(nd, vectorize="nodes")
+nd = c.network_def()
+
+compute_graph = ComputeGraph(nd, vectorize="none")
 
 result, _ = compute_graph.run(10., outputs={"V": ("PC", "PRO.0", "PSP")})
 
