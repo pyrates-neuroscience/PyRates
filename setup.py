ac31ad24693c9051f4fc99c8b0a06ff1e11f8acf
drose <drose@cbs.mpg.de>
2018-12-20 13:49:26 +0100

    added classifiers and reordered setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+LIBRARY_VERSION = "0.5.0"
 
-setup(name='pyrates',
-      version='0.5.0',
-      description='Neural Network Modeling Framework',
-      long_description=long_description,
-      author='Richard Gast, Daniel Rose',
-      author_email='rgast@cbs.mpg.de',
-      license='GPLv3',
-      packages=find_packages(),
-      zip_safe=False,
-      python_requires='>=3.6',
-      install_requires=['numpy',
+PYRATES_TEAM = "Richard Gast, Daniel Rose"
+
+INSTALL_REQUIREMENTS = ['numpy',
                         'matplotlib',
                         'networkx',
                         'pandas',
@@ -23,4 +14,32 @@ setup(name='pyrates',
                         'scipy',
                         'seaborn',
                         'mne',
-                        'pydot'])
+                        'pydot']
+
+CLASSIFIERS = ["Programming Language :: Python :: 3",
+               "Programming Language :: Python :: 3.6",
+               "Programming Language :: Python :: 3.7",
+               "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+               "Operating System :: OS Independent",
+               "Development Status :: 3 - Alpha",
+               "Intended Audience :: Developers",
+               "Intended Audience :: Science/Research",
+               "Topic :: Scientific/Engineering",
+               ]
+
+with open("README.md", "r") as fh:
+    DESCRIPTION = fh.read()
+
+setup(name='pyrates',
+      version=LIBRARY_VERSION,
+      description='Neural Network Modeling Framework',
+      long_description=DESCRIPTION,
+      author=PYRATES_TEAM,
+      author_email='rgast@cbs.mpg.de',
+      license='GPL v3',
+      packages=find_packages(),
+      zip_safe=False,
+      python_requires='>=3.6',
+      install_requires=INSTALL_REQUIREMENTS,
+      classifiers=CLASSIFIERS
+      )
