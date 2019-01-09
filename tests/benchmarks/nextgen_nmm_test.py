75d3fdec6f16fe93a1817550e96c2242e8c996f4
ch-salomon <ch.salomon@arcor.de>
2019-01-09 18:17:17 +0100

    Implementation of cluster_grid_search() functionality Added add_status_flag field to linearize_grid() Added function body for fetch_param()

--- tests/benchmarks/nextgen_nmm_test.py
+++ tests/benchmarks/nextgen_nmm_test.py
@@ -1,6 +1,7 @@
 from pyrates.frontend.template.circuit.circuit import CircuitTemplate
 from pyrates.backend import ComputeGraph
 from pyrates.utility import plot_timeseries, grid_search
+from pyrates.utility.grid_search import cluster_grid_search
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas import DataFrame
@@ -17,14 +18,19 @@ param_map = {'J_e': {'var': [('Op_e.0', 'J_e'), ('Op_i.0', 'J_e')],
                      'nodes': ['PC.0', 'IIN.0']}
              }
 
-# perform simulation
-results = grid_search(circuit_template="pyrates.examples.simple_nextgen_NMM.Net5",
+results = cluster_grid_search(hostnames=None, circuit_template="pyrates.examples.simple_nextgen_NMM.Net5",
                       param_grid=params, param_map=param_map,
                       inputs={("PC", "Op_e.0", "inp"): inp}, outputs={"r": ("PC", "Op_e.0", "r")},
                       dt=dt, simulation_time=T, permute_grid=True)
 
-# plotting
-for j_e in params['J_e']:
-    for j_i in params['J_i']:
-        ax = plot_timeseries(results[j_e][j_i], title=f"J_e={j_e}, J_i={j_i}")
-        plt.show()
+# # perform simulation
+# results = grid_search(circuit_template="pyrates.examples.simple_nextgen_NMM.Net5",
+#                       param_grid=params, param_map=param_map,
+#                       inputs={("PC", "Op_e.0", "inp"): inp}, outputs={"r": ("PC", "Op_e.0", "r")},
+#                       dt=dt, simulation_time=T, permute_grid=True)
+#
+# # plotting
+# for j_e in params['J_e']:
+#     for j_i in params['J_i']:
+#         ax = plot_timeseries(results[j_e][j_i], title=f"J_e={j_e}, J_i={j_i}")
+#         plt.show()
