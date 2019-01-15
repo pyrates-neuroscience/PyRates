f6b03f0ea9254146375416ed4ad5363ebd3b51a9
ch-salomon <ch.salomon@arcor.de>
2019-01-15 14:38:42 +0100

    Implementation of cluster_grid_search() and utility functions spawn_thread(), thread_master(), create_ssh_client() and fetch_param_idx(). Cluster_worker.py is called on the remote hosts during cluster_grid_search()

--- pyrates/utility/cluster_worker.py
+++ pyrates/utility/cluster_worker.py
@@ -1,53 +1,29 @@
-# # system imports
+# system imports
 import argparse
 import sys
 import ast
-import json
 
 # external imports
 import pandas as pd
+
 from pyrates.utility import grid_search
 
 
-def dummy():
-    x = 0
-    for i in range(200000000):
-        x = x + 1
-
-
 def main(_):
-    with open(FLAGS.config_file) as file:
-        param_dict = json.load(file)
-        try:
-            circuit_template = param_dict['circuit_template']
-            param_map = param_dict['param_map']
+    circuit_template = FLAGS.circuit_template
 
-            # Recreate tuple from string representation to use as 'key' in inputs
-            inputs = {ast.literal_eval(*param_dict['inputs'].keys()):
-                      list(*param_dict['inputs'].values())}
+    # Recreate dictionaries from their string representation
+    param_grid = FLAGS.param_grid
+    sampling_step_size = FLAGS.sampling_step_size
+    dt = FLAGS.dt
+    simulation_time = FLAGS.simulation_time
 
-            # Recreate tuple from list to use as 'values' in outputs
-            outputs = {str(*param_dict['outputs'].keys()):
-                       tuple(*param_dict['outputs'].values())}
+    # TODO: Recreate 'param_map', 'inputs' and 'outputs' from their string representation
+    param_map = ast.literal_eval(FLAGS.param_map)
+    inputs = ast.literal_eval(FLAGS.inputs)
+    outputs = ast.literal_eval(FLAGS.outputs)
 
-            sampling_step_size = param_dict['sampling_step_size']
-            dt = param_dict['dt']
-            simulation_time = param_dict['simulation_time']
-            result_path = FLAGS.result_path
-        except KeyError as err:
-            # If config_file does not contain any of the necessary keys
-            print("KeyError:", err)
-            return
-
-    # Recreate param_grid{} from its string representation and create a DataFrame from it
-    param_grid = pd.DataFrame(ast.literal_eval(FLAGS.param_grid_arg))
-
-    # Recreate fetched indices
-    param_grid_row_idx = param_grid.index.tolist()
-
-    # TODO: Await a param_grid from stdin to start grid_search()
-
-    # Exclude 'status'-key from param_grid because grid_search() can't handle the additional keyword
+    # Exclude 'status'-key param_grid because grid_search() can't handle the additional keyword
     param_grid_arg = param_grid.loc[:, param_grid.columns != "status"]
 
     results = grid_search(circuit_template=circuit_template,
@@ -59,19 +35,7 @@ def main(_):
                           dt=dt,
                           simulation_time=simulation_time)
 
-    # TODO: Write name of used config file and parameter combination to each result file
-    # Write each result to a separate file
-    # for col_idx, series in results.iteritems():
-
-    # for idx in range(len(results.columns)):
-    #     grid_idx = param_grid_row_idx[idx]
-    #     temp = pd.DataFrame(results.iloc[:, idx])
-    #     print(temp)
-        # file = f'/data/hu_salomon/Documents/ClusterGridSearch/Results/test_gridIdx_{grid_idx}.csv'
-        # print(f'Writing results to: {file}')
-        # temp.to_csv(file, index=False)
-
-    # TODO: Send output_file back to host if there is no shared memory space available
+    print(results.to_string(index=False))
 
 
 if __name__ == "__main__":
@@ -79,24 +43,58 @@ if __name__ == "__main__":
     # parser.register("type", "bool", lambda v: v.lower() == "true")
 
     parser.add_argument(
-        "--param_grid_arg",
+        "--circuit_template",
         type=str,
         default="",
-        help="String representation of parameter grid chunk for grid_search()"
+        help=""
     )
 
     parser.add_argument(
-        "--config_file",
+        "--param_grid",
         type=str,
         default="",
-        help="JSON file containing all necessary input to invoke grid_search()"
+        help=""
     )
 
     parser.add_argument(
-        "--result_path",
+        "--param_map",
         type=str,
         default="",
-        help="Path to save result files to"
+        help=""
+    )
+
+    parser.add_argument(
+        "--inputs",
+        type=str,
+        default="",
+        help=""
+    )
+
+    parser.add_argument(
+        "--outputs",
+        type=str,
+        default="",
+        help=""
+    )
+
+    parser.add_argument(
+        "--sampling_step_size",
+        default=None,
+        help=""
+    )
+
+    parser.add_argument(
+        "--dt",
+        type=float,
+        default=0.0,
+        help=""
+    )
+
+    parser.add_argument(
+        "--simulation_time",
+        type=float,
+        default=0.0,
+        help=""
     )
 
     FLAGS = parser.parse_args()
