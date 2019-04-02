a3575aee322b620163de1805892ca9467e80ee6b
drose <drose@cbs.mpg.de>
2019-04-02 17:42:34 +0200

    restricted tensorflow version to >=1.12 <= 1.13 for now

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ INSTALL_REQUIREMENTS = ['numpy',
                         'scipy',
                         'seaborn',
                         'mne',
-                        'tensorflow >=1.12, <2.0',
+                        'tensorflow >=1.12, <=1.13',
                         'pydot']
 
 CLASSIFIERS = ["Programming Language :: Python :: 3",
