24b5c004f02db1dc0f4e8cef8a470aeafd62e4c1
rgast <rgast@cbs.mpg.de>
2019-07-03 20:46:53 +0200

    slight modification of conf.py so readthedocs can find our logo

--- documentation/doc/source/conf.py
+++ documentation/doc/source/conf.py
@@ -81,7 +81,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../build/html/_static']
+html_static_path = ['documentation/doc/build/html/_static']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
