894fd0781a2a3dec454633ca23f078e258c073fe
rgast <rgast@cbs.mpg.de>
2019-09-02 12:57:38 +0200

    Adjusted grid_search to store parameter combinations in a ordered way (dataframe) that works better together with ClusterGridsearch

--- documentation/coupled_jrcs.py
+++ documentation/coupled_jrcs.py
@@ -1,5 +1,6 @@
 # pyrates imports
 from pyrates.utility import grid_search
+from pyrates.utility.grid_search import linearize_grid
 
 # additional imports
 import numpy as np
@@ -29,7 +30,7 @@ T = 1.0                                                         # overall simula
 inp1 = np.random.uniform(120., 320., (int(T/dt), 1))            # white noise input to the pyramidal cells in Hz.
 inp2 = np.random.uniform(120., 320., (int(T/dt), 1))
 
-N = 100                                                         # grid-size
+N = 10                                                          # grid-size
 C = np.linspace(0., 200., N)                                    # bi-directional connection strength
 D = np.linspace(0., 1e-2, N)                                    # bi-directional coupling delay
 
@@ -40,30 +41,29 @@ param_map = {'C': {'vars': ['weight'],
              'D': {'vars': ['delay'],
                    'edges': [('JRC1/PC', 'JRC2/PC'), ('JRC2/PC', 'JRC1/PC')]}}
 
-
 # grid searches
 ###############
 
 # numpy backend grid-search
-# results, param_map, _ = grid_search(circuit_template="model_templates.jansen_rit.simple_jansenrit.JRC_delaycoupled",
-#                                     param_grid=params, param_map=param_map,
-#                                     inputs={"JRC1/PC/RPO_e_pc/u": np.asarray(inp1, dtype=np.float32),
-#                                             "JRC2/PC/RPO_e_pc/u": np.asarray(inp2, dtype=np.float32)},
-#                                     outputs={"v": "all/PC/OBS/V"},
-#                                     dt=dt, simulation_time=T, permute_grid=True, sampling_step_size=1e-3,
-#                                     init_kwargs={'solver': 'euler', 'backend': 'numpy'}, profile='t',
-#                                     njit=True
-#                                     )
-
-# tensorflow backend grid-search
 results, param_map, _ = grid_search(circuit_template="model_templates.jansen_rit.simple_jansenrit.JRC_delaycoupled",
                                     param_grid=params, param_map=param_map,
                                     inputs={"JRC1/PC/RPO_e_pc/u": np.asarray(inp1, dtype=np.float32),
                                             "JRC2/PC/RPO_e_pc/u": np.asarray(inp2, dtype=np.float32)},
                                     outputs={"v": "all/PC/OBS/V"},
                                     dt=dt, simulation_time=T, permute_grid=True, sampling_step_size=1e-3,
-                                    init_kwargs={'solver': 'euler', 'backend': 'tensorflow'}, profile='t',
+                                    init_kwargs={'solver': 'euler', 'backend': 'numpy'}, profile='t',
+                                    njit=True
                                     )
 
-#results.plot()
-#plt.show()
+# tensorflow backend grid-search
+# results, param_map, _ = grid_search(circuit_template="model_templates.jansen_rit.simple_jansenrit.JRC_delaycoupled",
+#                                     param_grid=params, param_map=param_map,
+#                                     inputs={"JRC1/PC/RPO_e_pc/u": np.asarray(inp1, dtype=np.float32),
+#                                             "JRC2/PC/RPO_e_pc/u": np.asarray(inp2, dtype=np.float32)},
+#                                     outputs={"v": "all/PC/OBS/V"},
+#                                     dt=dt, simulation_time=T, permute_grid=True, sampling_step_size=1e-3,
+#                                     init_kwargs={'solver': 'euler', 'backend': 'tensorflow'}, profile='t',
+#                                     )
+
+results.plot()
+plt.show()
