10d8299edc6b720294fa1cc16acd91c05c5fc18a
anguisterrenis <stormfalcon@gmx.de>
2020-01-06 21:00:43 +0100

    increment version number to 0.9.0

--- pyrates/__init__.py
+++ pyrates/__init__.py
@@ -32,7 +32,7 @@ i.e. networks of nodes connected via edges.
 
 __author__ = "Richard Gast, Daniel Rose"
 __status__ = "Development"
-__version__ = "0.8.2"
+__version__ = "0.9.0"
 
 
 class PyRatesException(Exception):
