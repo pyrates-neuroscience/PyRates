f95e7bd050a4f6824d0d68488c700f2e2293dad5
Daniel F. Rose <dafrose@users.noreply.github.com>
2020-04-19 23:46:55 +0200

    Fix for travis (#20)
    travis starts tests in the base environment instead of in the tests/ folder. Fixed that by setting up a fixture that sets and recovers the correct directory. this is a squash commit of the following commits:
    
    * added error handling in file comparison after pickle dump test
    
    * added print statement in fixture to analyse where tests are executed.
    
    * changed print to warning
    
    * added directory change to test directory
    
    * moved directory change to separate fixture at session scope
    
    * removed warnings
    
    * added autouse to session fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,19 @@
 """Pytest module-wide configuration file."""
 
 import pytest
+import os
+
+
+@pytest.fixture(scope="session", autouse=True)
+def ensure_test_directory():
+    if os.getcwd().endswith("PyRates"):
+        os.chdir("tests/")
+
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
     # Code that will run before your test
     # Find and save current (test) working directory
-    import os
     cwd = os.getcwd()
     # A test function will be run at this point
     yield
