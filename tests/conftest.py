1897aa8613534bf0845becef88552663ac262f83
Daniel Rose <drose@cbs.mpg.de>
2020-04-19 23:25:02 +0200

    removed warnings

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,13 +14,9 @@ def ensure_test_directory():
 def run_around_tests():
     # Code that will run before your test
     # Find and save current (test) working directory
-    import os
-    import warnings
     cwd = os.getcwd()
-    warnings.warn(f"before: {cwd}")
     # A test function will be run at this point
     yield
     # Code that will run after the test
-    warnings.warn(f"\n after: {os.getcwd()}")
     # revert changes to working directory
     os.chdir(cwd)
