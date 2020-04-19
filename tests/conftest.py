494b3e8c9ce7bcdf70360bb88f6f8b551f1c7b73
Daniel Rose <drose@cbs.mpg.de>
2020-04-19 20:11:51 +0200

    fixing tests: moved all pyrates imports into test functions and added fixture that preserves working directory before/after a given test.

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+"""Pytest module-wide configuration file."""
+
+import pytest
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    # Code that will run before your test
+    # Find and save current (test) working directory
+    import os
+    cwd = os.getcwd()
+    # A test function will be run at this point
+    yield
+    # Code that will run after the test
+    # revert changes to working directory
+    os.chdir(cwd)
