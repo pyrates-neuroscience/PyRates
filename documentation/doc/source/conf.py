526328f697fd260ae7483b02f559c8061fab54ac
rgast <rgast@cbs.mpg.de>
2022-01-14 11:28:53 -0600

    - small debugging update to the readthedocs build config - TODO(high priority): Implement changes in the documentation and utility functions - TODO(high priority): Clean-up and document backend/frontend implementations (consider re-organizing the caches to make it easier to clear them)

--- documentation/doc/source/conf.py
+++ documentation/doc/source/conf.py
@@ -62,9 +62,9 @@ sphinx_gallery_conf = {
         '../../model_analysis',
     ],   # path to your example scripts
     'gallery_dirs': [
-        'auto_introductions',
-        'auto_implementations',
-        'auto_analysis',
+        '../build/auto_introductions',
+        '../build/auto_implementations',
+        '../build/auto_analysis',
     ]  # path to where to save gallery generated output
 
 }
