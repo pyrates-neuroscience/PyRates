36bdc7478bb7bdb9ddeb2e038a90ec05c7876dbe
rgast <rgast@cbs.mpg.de>
2022-01-14 11:14:14 -0600

    - removed the RepresentationBase class from utility for now. Its not used and not compatible with Python 3.6 - TODO(high priority): Implement changes in the documentation and utility functions - TODO(high priority): Clean-up and document backend/frontend implementations (consider re-organizing the caches to make it easier to clear them)

--- pyrates/utility.py
+++ pyrates/utility.py
@@ -33,13 +33,13 @@ __author__ = "Christoph Salomon, Richard Gast"
 __status__ = "development"
 
 # external imports
-from typing import Union, Optional, Generator, Tuple, Any, OrderedDict, Dict
+from typing import Union, Optional, Tuple, Dict
 import pandas as pd
 import numpy as np
 from copy import deepcopy
 import json
 from inspect import getsource
-from networkx import node_link_data
+#from networkx import node_link_data
 from pandas import DataFrame
 import pickle
 
@@ -333,133 +333,133 @@ class CustomEncoder(json.JSONEncoder):
             return super().default(obj)
 
 
-class RepresentationBase(object):
-    """Class that implements a __repr__ that yields the __init__ function signature with provided arguments in the
-    form 'module.Class(arg1, arg2, arg3)'"""
-
-    def __new__(cls, *args, **kwargs):
-
-        # noinspection PyArgumentList
-        instance = super().__new__(cls)
-        _init_dict = {**kwargs}
-        if args:
-            _init_dict["args"] = args
-
-        instance._init_dict = _init_dict
-
-        return instance
-
-    def __repr__(self) -> str:
-        """Magic method that returns to repr(object). It retrieves the signature of __init__ and maps it to class
-        attributes to return a representation in the form 'module.Class(arg1=x, arg2=y, arg3=3)'. Raises AttributeError,
-        if the a parameter in the signature cannot be found (is not saved). The current implementation """
-
-        # repr_str = f"{self.__module__!r}.{self.__class__!r}("
-        # params = self._get_params()
-        from copy import copy
-        init_dict = copy(self._init_dict)
-
-        # args = ""
-        # if "args" in init_dict:
-        #     args = init_dict.pop("args", None)
-        #     args = ", ".join((f"{value!r}" for value in args))
-        #
-        # kwargs = ", ".join((f"{name}={value!r}" for name, value in init_dict.items()))
-        # param_str = f"{args}, {kwargs}"
-        param_str = "*args, **kwargs"
-        _module = self.__class__.__module__
-        _class = self.__class__.__name__
-        # return f"{_module}.{_class}({param_str})"
-
-        try:
-            return f"{_class} '{self.key}'"
-        except AttributeError:
-            return f"{_class}"
-
-    def _defaults(self) -> Generator[Tuple[str, Any], None, None]:
-        """Inspects the __init__ special method and yields tuples of init parameters and their default values."""
-
-        import inspect
-
-        # retrieve signature of __init__ method
-        sig = inspect.signature(self.__init__)
-
-        # retrieve parameters in the signature
-        for name, param in sig.parameters.items():
-            # check if param has a default value
-            if np.all(param.default != inspect.Parameter.empty):
-                # yield parameter name and default value
-                yield name, param.default
-
-    def to_dict(self, include_defaults=False, include_graph=False, recursive=False) -> OrderedDict:
-        """Parse representation string to a dictionary to later convert it to json."""
-
-        _dict = dict()
-        _dict["class"] = {"__module__": self.__class__.__module__, "__name__": self.__class__.__name__}
-        # _dict["module"] = self.__class__.__module__
-        # _dict["class"] = self.__class__.__name__
-
-        # obtain parameters that were originally passed to __init__
-        ###########################################################
-
-        for name, value in self._init_dict.items():
-            _dict[name] = value
-
-        # include defaults of parameters that were not specified when __init__ was called.
-        ##################################################################################
-
-        if include_defaults:
-            default_dict = {}
-            for name, value in self._defaults():
-                if name not in _dict:  # this fails to get positional arguments that override defaults to keyword args
-                    default_dict[name] = value
-            _dict["defaults"] = default_dict
-
-        # Include graph if desired
-        ##########################
-
-        # check if it quacks like a duck... eh.. has a network_graph like a circuit
-        if include_graph and hasattr(self, "network_graph"):
-            net_dict = node_link_data(self.network_graph)
-            if recursive:
-                for node in net_dict["nodes"]:
-                    node["data"] = node["data"].to_dict(include_defaults=include_defaults, recursive=True)
-            _dict["network_graph"] = net_dict
-
-        # Apply dict transformation recursively to all relevant objects
-        ###############################################################
-
-        if recursive:
-            for key, item in _dict.items():
-                # check if it quacks like a duck... eh.. represents like a RepresentationBase
-                # note, however, that "to_dict" is actually implemented by a number of objects outside this codebase
-                if hasattr(item, "to_dict"):
-                    _dict[key] = item.to_dict(include_defaults=include_defaults, recursive=True)
-
-        return _dict
-
-    def to_json(self, include_defaults=False, include_graph=False, path="", filename=""):
-        """Parse a dictionary into """
-
-        # from pyrates.utility.json_filestorage import CustomEncoder
-
-        _dict = self.to_dict(include_defaults=include_defaults, include_graph=include_graph, recursive=True)
-
-        if filename:
-            import os
-            filepath = os.path.join(path, filename)
-
-            # create directory if necessary
-            create_directory(filepath)
-
-            if not filepath.lower().endswith(".json"):
-                filepath = f"{filepath}.json"
-
-            with open(filepath, "w") as outfile:
-                json.dump(_dict, outfile, cls=CustomEncoder, indent=4)
-
-        # return json as string
-        return json.dumps(_dict, cls=CustomEncoder, indent=2)
+# class RepresentationBase(object):
+#     """Class that implements a __repr__ that yields the __init__ function signature with provided arguments in the
+#     form 'module.Class(arg1, arg2, arg3)'"""
+#
+#     def __new__(cls, *args, **kwargs):
+#
+#         # noinspection PyArgumentList
+#         instance = super().__new__(cls)
+#         _init_dict = {**kwargs}
+#         if args:
+#             _init_dict["args"] = args
+#
+#         instance._init_dict = _init_dict
+#
+#         return instance
+#
+#     def __repr__(self) -> str:
+#         """Magic method that returns to repr(object). It retrieves the signature of __init__ and maps it to class
+#         attributes to return a representation in the form 'module.Class(arg1=x, arg2=y, arg3=3)'. Raises AttributeError,
+#         if the a parameter in the signature cannot be found (is not saved). The current implementation """
+#
+#         # repr_str = f"{self.__module__!r}.{self.__class__!r}("
+#         # params = self._get_params()
+#         from copy import copy
+#         init_dict = copy(self._init_dict)
+#
+#         # args = ""
+#         # if "args" in init_dict:
+#         #     args = init_dict.pop("args", None)
+#         #     args = ", ".join((f"{value!r}" for value in args))
+#         #
+#         # kwargs = ", ".join((f"{name}={value!r}" for name, value in init_dict.items()))
+#         # param_str = f"{args}, {kwargs}"
+#         param_str = "*args, **kwargs"
+#         _module = self.__class__.__module__
+#         _class = self.__class__.__name__
+#         # return f"{_module}.{_class}({param_str})"
+#
+#         try:
+#             return f"{_class} '{self.key}'"
+#         except AttributeError:
+#             return f"{_class}"
+#
+#     def _defaults(self) -> Generator[Tuple[str, Any], None, None]:
+#         """Inspects the __init__ special method and yields tuples of init parameters and their default values."""
+#
+#         import inspect
+#
+#         # retrieve signature of __init__ method
+#         sig = inspect.signature(self.__init__)
+#
+#         # retrieve parameters in the signature
+#         for name, param in sig.parameters.items():
+#             # check if param has a default value
+#             if np.all(param.default != inspect.Parameter.empty):
+#                 # yield parameter name and default value
+#                 yield name, param.default
+#
+#     def to_dict(self, include_defaults=False, include_graph=False, recursive=False) -> OrderedDict:
+#         """Parse representation string to a dictionary to later convert it to json."""
+#
+#         _dict = dict()
+#         _dict["class"] = {"__module__": self.__class__.__module__, "__name__": self.__class__.__name__}
+#         # _dict["module"] = self.__class__.__module__
+#         # _dict["class"] = self.__class__.__name__
+#
+#         # obtain parameters that were originally passed to __init__
+#         ###########################################################
+#
+#         for name, value in self._init_dict.items():
+#             _dict[name] = value
+#
+#         # include defaults of parameters that were not specified when __init__ was called.
+#         ##################################################################################
+#
+#         if include_defaults:
+#             default_dict = {}
+#             for name, value in self._defaults():
+#                 if name not in _dict:  # this fails to get positional arguments that override defaults to keyword args
+#                     default_dict[name] = value
+#             _dict["defaults"] = default_dict
+#
+#         # Include graph if desired
+#         ##########################
+#
+#         # check if it quacks like a duck... eh.. has a network_graph like a circuit
+#         if include_graph and hasattr(self, "network_graph"):
+#             net_dict = node_link_data(self.network_graph)
+#             if recursive:
+#                 for node in net_dict["nodes"]:
+#                     node["data"] = node["data"].to_dict(include_defaults=include_defaults, recursive=True)
+#             _dict["network_graph"] = net_dict
+#
+#         # Apply dict transformation recursively to all relevant objects
+#         ###############################################################
+#
+#         if recursive:
+#             for key, item in _dict.items():
+#                 # check if it quacks like a duck... eh.. represents like a RepresentationBase
+#                 # note, however, that "to_dict" is actually implemented by a number of objects outside this codebase
+#                 if hasattr(item, "to_dict"):
+#                     _dict[key] = item.to_dict(include_defaults=include_defaults, recursive=True)
+#
+#         return _dict
+#
+#     def to_json(self, include_defaults=False, include_graph=False, path="", filename=""):
+#         """Parse a dictionary into """
+#
+#         # from pyrates.utility.json_filestorage import CustomEncoder
+#
+#         _dict = self.to_dict(include_defaults=include_defaults, include_graph=include_graph, recursive=True)
+#
+#         if filename:
+#             import os
+#             filepath = os.path.join(path, filename)
+#
+#             # create directory if necessary
+#             create_directory(filepath)
+#
+#             if not filepath.lower().endswith(".json"):
+#                 filepath = f"{filepath}.json"
+#
+#             with open(filepath, "w") as outfile:
+#                 json.dump(_dict, outfile, cls=CustomEncoder, indent=4)
+#
+#         # return json as string
+#         return json.dumps(_dict, cls=CustomEncoder, indent=2)
 
 
 def get_simulation_data(circuit, state_variable='membrane_potential', pop_keys: Union[tuple, list] = None,
