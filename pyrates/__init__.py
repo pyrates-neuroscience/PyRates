4cd7e286b2aeef39d56bd4fef7dfb2aefc78ce1b
rgast <rgast@cbs.mpg.de>
2022-01-17 17:41:47 -0600

    Bump version: 0.10.0-dev1 → 0.10.0

--- pyrates/__init__.py
+++ pyrates/__init__.py
@@ -32,7 +32,7 @@ i.e. networks of nodes connected via edges.
 
 __author__ = "Richard Gast, Daniel Rose"
 __status__ = "Development"
-__version__ = "0.10.0-dev1"
+__version__ = "0.10.0"
 
 
 from pyrates.utility import integrate, circuit_from_yaml, circuit_from_pickle, grid_search, clear_frontend_caches
