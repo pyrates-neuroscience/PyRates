a4ca06b9c7d5e1ebe0e4b00cc64a14be61c1cbdd
Richert <richgast.90@gmail.com>
2022-09-22 17:15:53 -0500

    - updated minor version in the sphinx `conf.py`

--- documentation/doc/source/conf.py
+++ documentation/doc/source/conf.py
@@ -25,7 +25,7 @@ copyright = u'2022, Richard Gast and Daniel Rose'
 author = u'Richard Gast and Daniel Rose'
 
 # The short X.Y version
-version = u'0.15'
+version = u'0.16'
 
 # The full version, including alpha/beta/rc tags
 release = u'0.16.0'  # automatically managed with bumpversion, see .bumpversion.cfg
