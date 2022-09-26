4506dd43871459374289dc680f28a3b566bd8253
Richert <richgast.90@gmail.com>
2022-09-26 12:11:51 -0500

    Bump version: 0.17.0-dev0 → 0.17.0

--- pyrates/__init__.py
+++ pyrates/__init__.py
@@ -32,7 +32,7 @@ i.e. networks of nodes connected via edges.
 
 __author__ = "Richard Gast, Daniel Rose"
 __status__ = "Development"
-__version__ = "0.17.0-dev0"
+__version__ = "0.17.0"
 
 
 from pyrates.utility import integrate, grid_search, clear_frontend_caches, clear
