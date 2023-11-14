18a3335d454a078b37ca0863ae7ee0dd23c9f8b3
Richert <richgast.90@gmail.com>
2023-11-14 12:15:41 -0600

    dropped official support for python 3.6 and added support for python 3.10

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ INSTALL_REQUIREMENTS = ['numpy',
                         ]
 
 CLASSIFIERS = ["Programming Language :: Python :: 3",
-               "Programming Language :: Python :: 3.6",
                "Programming Language :: Python :: 3.7",
                "Programming Language :: Python :: 3.8",
                "Programming Language :: Python :: 3.9",
+               "Programming Language :: Python :: 3.10",
                "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
                "Operating System :: OS Independent",
                "Development Status :: 3 - Alpha",
